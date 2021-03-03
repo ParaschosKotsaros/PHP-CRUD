443e1816382a8b99e4e99da18879baa2c9633717
ParaschosKotsaros <48206026+ParaschosKotsaros@users.noreply.github.com>
2021-03-03 22:15:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud','root','');
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
 $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
 $statement->execute();
 $products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
 
 ?>
 
@@ -41,18 +44,26 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
   foreach($products as $i=>$product):  ?>
  <tr>
       <th scope="row"><?php echo $i + 1 ?></th>
-    
       <td><?php  echo $product['title'];?></td>
       <td><?php  echo $product['price'];?></td>
       <td><?php  echo $product['img'] ?></td>
       <td><?php  echo $product['create_date'] ?></td>
        <td>
        <button type="button" class="btn btn-outline-primary">Edit</button>
-       <button type="button" class="btn btn-outline-danger">Delete</button>
+
+       <form style= "display: inline-block"  method="post" action="delete.php">
+       <input type="hidden" name='id' value=" <?php echo $product['id']; ?>">
+       <button type="submit" class="btn btn-outline-danger">Delete</button>
+       </form>
        </td>
     </tr>
 
  <?php endforeach; ?>
+
+<form action="createproduct.php">
+ <button class="btn btn-outline-primary">Add New Product </button>
+ </form>
+ 
 
     
     
